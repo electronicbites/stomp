ae26d8c7ef2f782c073e031b0f642a2b2f02ddd0
Martin Woeginger <martin.woeginger@gmail.com>
2011-08-10 13:47:17 +0200

    fixed example with rabbit mq: create just a plain client. original code throws error in rabbit:
    =ERROR REPORT==== 10-Aug-2011::13:33:57 ===
    STOMP error frame sent:
    Message: "Processing error"
    Detail: "Processing error\n"
    Server private detail: {{badmatch,{error,auth_failure}},
                            [{rabbit_stomp_processor,do_login,6},
                             {rabbit_stomp_processor,process_request,3},
                             {gen_server2,handle_msg,2},
                             {proc_lib,init_p_do_apply,3}]}

--- examples/publisher.rb
+++ examples/publisher.rb
@@ -3,12 +3,13 @@ require 'stomp'
 
 #client = Stomp::Client.new("", "", "localhost", 61613)
 
-client = Stomp::Client.new("failover://(stomp://:@localhost:61613,stomp://:@remotehost:61613)?initialReconnectDelay=5000&randomize=false&useExponentialBackOff=false")
+#client = Stomp::Client.new("failover://(stomp://:@localhost:61613,stomp://:@remotehost:61613)?initialReconnectDelay=5000&randomize=false&useExponentialBackOff=false")
+client = Stomp::Client.new("guest", "guest", "localhost", 61613)
 message = "ronaldo #{ARGV[0]}"
 
 for i in (1..300)
   puts "Sending message"
-  client.send("/queue/ronaldo", "#{i}: #{message}", {:persistent => true})
+  client.publish("/queue/ronaldo", "#{i}: #{message}", {:persistent => true})
   puts "(#{Time.now})Message sent: #{i}"
   sleep 1
 end
